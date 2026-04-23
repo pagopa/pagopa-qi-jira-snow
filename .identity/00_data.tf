bb264a99ecab9e5cabd62f36c121b2796adaf572
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2026-04-23 15:46:51 +0200

    feat: [PIDM-1599] add deploy slack webhook config

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -53,3 +53,8 @@ data "azurerm_user_assigned_identity" "workload_identity_clientid" {
   resource_group_name = "pagopa-${var.env_short}-weu-${var.env}-aks-rg"
 }
 
+data "azurerm_key_vault_secret" "key_vault_deploy_slack_webhook" {
+  name         = "pagopa-pagamenti-deploy-slack-webhook"
+  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
+}
+
